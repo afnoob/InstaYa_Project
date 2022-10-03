4ab00f87af45022703c7da1d90b76241f7eb44cd
andreslcaicedo <andreslcaicedo@gmail.com>
2022-10-03 00:32:53 -0500

    Correccion get en datos de usuario

--- backend/routes/userdata.js
+++ backend/routes/userdata.js
@@ -6,7 +6,7 @@ router.get("/user-data/:username", async (request, response) =>{
     let username = request.params.username
     User.findOne({"User":username}).then(doc => {
         if (!doc) { return response.status(404).end();}
-        return response.status(200).json(doc);
+        return response.status(200).json(doc._id);
     })
     .catch(err => next(err));
 });
