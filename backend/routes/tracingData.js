0493b60de58916192315e13103d1bda31425b30a
Dierick Brochero <71480347+Dierickb@users.noreply.github.com>
2022-10-03 21:41:57 -0500

    fix: fix tracing data return doc.id_ to doc

--- backend/routes/tracingData.js
+++ backend/routes/tracingData.js
@@ -6,7 +6,7 @@ router.get("/actualizar-orden/:item_tracing", async (request, response) =>{
     let tracing = request.params.item_tracing;
     Order.findOne({"Tracing":tracing}).then(doc => {
         if (!doc) { return response.status(404).end();}
-        return response.status(200).json(doc._id);
+        return response.status(200).json(doc);
     })
         .catch(err => next(err));
 });
