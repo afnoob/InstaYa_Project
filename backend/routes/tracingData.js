8d7dfda57286448058f86f371f6ab0e7935bd148
Dierick Brochero <71480347+Dierickb@users.noreply.github.com>
2022-10-03 23:58:20 -0500

    feat: add update orders backend

--- backend/routes/tracingData.js
+++ backend/routes/tracingData.js
@@ -11,4 +11,30 @@ router.get("/actualizar-orden/:item_tracing", async (request, response) =>{
         .catch(err => next(err));
 });
 
+router.post('/actualizar-orden/:item_tracing', async (request, response) =>{
+    let tasks = request.body;
+    let tracing = request.params.item_tracing;
+    let tasksKeys = Object.keys(tasks);
+    try {
+        const findOrder = await Order.findOne({"Tracing":tracing});
+        
+        for (element of tasksKeys) {
+            if ( tasks[element] !== findOrder[element] && tasks[element] !== "") {
+                findOrder[element] = tasks[element];
+            }
+        }
+        
+        await Order.findOneAndUpdate({"Tracing":tracing}, findOrder);
+        
+        response.status(201).send({message:"Orden actualizada"});
+    } catch (error) {
+        console.log(error.message)
+        response.status(500).send({message:"Error al actualizar la orden"})
+    }
+})
+
+const validateUpdateOrder = (data) => {
+    
+}
+
 module.exports = router
